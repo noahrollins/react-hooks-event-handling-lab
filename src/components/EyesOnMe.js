43aed5ce5d7e7859b9ec0f2996827bea041fd4e6
Noah Rollins <noahjrollins@gmail.com>
2022-11-22 09:53:55 -0600

    commit tree 031b6258764a2b35db48265a4cb18af54d5c3e70

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+function focused(event) {
+    console.log('Good!')
+} 
+
+function blurred(event) {
+    console.log('Hey! Eyes on me!')
+} 
+
+
+function EyesOnMe (){
+    return (
+        <div>
+            <button onFocus={focused} onBlur={blurred}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
